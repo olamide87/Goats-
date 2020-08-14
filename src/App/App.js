9a846f685a3d546285eab8f44f5456bcdb12f916
Olamide Oladinni <olajide1987@gmail.com>
2020-08-13 19:24:43 -0500

    setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>APP COMPONENT HERE!!!!</h1>
+        <button className="btn btn-info">
+          <i className="fab fa-react"></i> Btn Here <i className="fab fa-react"></i>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
